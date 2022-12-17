dde6ae8bfc37655c8e819a03963a6ea199533b52
Kariuki33 <danqarees@gmail.com>
2022-12-17 02:44:39 -0800

    All Tasks

---
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -0,0 +1,5 @@
+#include "lists.h"
+
+/**
+ * insert_node - Inserts a number into a sorted singly-linkrd liat.
+ * @head: A pointer the head
---
+++ 0x01-python-if_else_loops_functions/README.md
@@ -0,0 +1,3 @@
+0x01-python-if_else_loops_functions
+
+The following are projects on and about if else loop functions in Python.
