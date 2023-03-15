29ad5ddf33eda26dcc27b18607a79d1655e12000
Kariuki33 <danqarees@gmail.com>
2023-03-15 18:08:39 +0300

    Update 102-concat.js

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -1,5 +1,13 @@
 #!/usr/bin/node
-const fs = require('fs');
-const src1 = fs.readFileSync(process.argv[2], 'utf8');
-const src2 = fs.readFileSync(process.argv[3], 'utf8');
-fs.writeFileSync(process.argv[4], src1 + src2);
+const { readFileSync, writeFile } = require('fs');
+const { argv } = require('process');
+
+const getContent = (file) => {
+  return readFileSync(file, 'utf8');
+};
+
+const concated = getContent(argv[2]) + '' + getContent(argv[3]);
+
+writeFile(argv[4], concated, 'utf8', err => {
+  if (err) throw err;
+});
